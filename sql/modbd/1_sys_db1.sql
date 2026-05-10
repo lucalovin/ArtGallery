e22a494a9d9689b60b8260dbde8a7d2e87d7e20a
George <nicola_george1999@yahoo.com>
2026-05-10 19:04:43 +0300

    update sus_db1 si 2

--- sql/modbd/1_sys_db1.sql
+++ sql/modbd/1_sys_db1.sql
@@ -36,7 +36,44 @@ CREATE PUBLIC DATABASE LINK link_global
 
 COMMIT;
 
+
+-- Drop link-urile create gre?it
+--DROP PUBLIC DATABASE LINK link_bddall;
+--DROP PUBLIC DATABASE LINK link_am;
+--DROP PUBLIC DATABASE LINK link_global;
+--DROP PUBLIC DATABASE LINK link_eu;
+
+
+-- Conectare fara tsnames.ora
+-- CREATE PUBLIC DATABASE LINK link_eu
+-- CONNECT TO ARTGALLERY_EU IDENTIFIED BY parola_eu
+-- USING '(DESCRIPTION=
+--            (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--            (CONNECT_DATA=(SERVICE_NAME=ORCLPDB2))
+--        )';
+ 
+-- CREATE PUBLIC DATABASE LINK link_am
+-- CONNECT TO ARTGALLERY_AM IDENTIFIED BY parola_am
+-- USING '(DESCRIPTION=
+--            (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--            (CONNECT_DATA=(SERVICE_NAME=ORCLPDB))
+--        )';
+ 
+-- CREATE PUBLIC DATABASE LINK link_global
+-- CONNECT TO ARTGALLERY_GLOBAL IDENTIFIED BY parola_global
+-- USING '(DESCRIPTION=
+--            (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--            (CONNECT_DATA=(SERVICE_NAME=ORCLPDB))
+--        )';
+ 
+-- COMMIT;
+
 -- Verificare
--- SELECT * FROM dual@link_eu;
--- SELECT * FROM dual@link_am;
--- SELECT * FROM dual@link_global;
+--  SELECT * FROM dual@link_eu;
+--  SELECT * FROM dual@link_am;
+--  SELECT * FROM dual@link_global;
+ 
+ 
+ 
+-- SELECT OWNER, DB_LINK, USERNAME, HOST, CREATED 
+-- FROM DBA_DB_LINKS;
--- sql/modbd/1_sys_db2.sql
+++ sql/modbd/1_sys_db2.sql
@@ -35,7 +35,51 @@ CREATE PUBLIC DATABASE LINK link_eu
 
 COMMIT;
 
+
+
+---- Drop link-urile create gre?it
+DROP PUBLIC DATABASE LINK link_am;
+DROP PUBLIC DATABASE LINK link_global;
+DROP PUBLIC DATABASE LINK link_bddall;
+DROP PUBLIC DATABASE LINK link_eu;
+
+
+-- Conectare fara tsnames.ora
+--CREATE PUBLIC DATABASE LINK link_am
+--CONNECT TO ARTGALLERY_AM IDENTIFIED BY parola_am
+--USING '(DESCRIPTION=
+--           (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--           (CONNECT_DATA=(SERVICE_NAME=ORCLPDB))
+--       )';
+-- 
+--CREATE PUBLIC DATABASE LINK link_global
+--CONNECT TO ARTGALLERY_GLOBAL IDENTIFIED BY parola_global
+--USING '(DESCRIPTION=
+--           (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--           (CONNECT_DATA=(SERVICE_NAME=ORCLPDB))
+--       )';
+-- 
+--CREATE PUBLIC DATABASE LINK link_bddall
+--CONNECT TO BDDALL IDENTIFIED BY "1234"
+--USING '(DESCRIPTION=
+--           (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--           (CONNECT_DATA=(SERVICE_NAME=ORCLPDB))
+--       )';
+-- 
+--CREATE PUBLIC DATABASE LINK link_eu
+--CONNECT TO ARTGALLERY_EU IDENTIFIED BY parola_eu
+--USING '(DESCRIPTION=
+--           (ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))
+--           (CONNECT_DATA=(SERVICE_NAME=ORCLPDB2))
+--       )';
+-- 
+--COMMIT;
+
+
 -- Verificare
 -- SELECT * FROM dual@link_am;
 -- SELECT * FROM dual@link_global;
 -- SELECT * FROM dual@link_bddall;
+-- 
+-- SELECT OWNER, DB_LINK, USERNAME, HOST, CREATED 
+--FROM DBA_DB_LINKS;
